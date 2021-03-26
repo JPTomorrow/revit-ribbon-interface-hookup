d65846c093fe4e4e28b48b4d30fc8f0854e36ab4
Justin Morrow <justin.parker.morrow@gmail.com>
2021-03-26 09:59:45 -0500

    cleanup and add P3Lighting

--- libs/InvokeClasses.cs
+++ libs/InvokeClasses.cs
@@ -66,11 +66,7 @@ namespace MainApp
 
 	//Conduit Projects
 	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-	public class InvokeSplitElementConduit : InvokeModuleBase, IExternalCommand { public override string ModuleName => "SplitElementConduit"; }
-	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
 	public class InvokeConduitRunInfo : InvokeModuleBase, IExternalCommand { public override string ModuleName => "ConduitRunInfo"; }
-	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-	public class InvokeSingleConduitRunInfo : InvokeModuleBase, IExternalCommand { public override string ModuleName => "SingleConduitRunInfo"; }
 	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
 	public class InvokeJboxMatchParams : InvokeModuleBase, IExternalCommand { public override string ModuleName => "JboxMatchParams"; }
 	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
@@ -80,47 +76,33 @@ namespace MainApp
 	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
 	public class InvokeConduitRunMatchParamService : InvokeModuleBase, IExternalCommand { public override string ModuleName => "ConduitRunMatchParamService"; }
 	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-	public class InvokeConduitTaggingFactory : InvokeModuleBase, IExternalCommand { public override string ModuleName => "ConduitTaggingFactory"; }
-	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-	public class InvokeConduitIdGenerator : InvokeModuleBase, IExternalCommand { public override string ModuleName => "ConduitIdGenerator"; }
-	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
 	public class InvokeSwapJboxParam : InvokeModuleBase, IExternalCommand { public override string ModuleName => "SwapJboxParam"; }
 	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-	public class InvokePanelScheduleInfo : InvokeModuleBase, IExternalCommand { public override string ModuleName => "PanelScheduleInf"; }
-	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-	public class InvokeAutoRouteBranchConduit : InvokeModuleBase, IExternalCommand { public override string ModuleName => "AutoRouteBranchConduit"; }
+	public class InvokePullBoxSizer : InvokeModuleBase, IExternalCommand { public override string ModuleName => "PullBoxSizer"; }
+	
 	//View Projects
-	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-	public class InvokeLinkedFileSettingsOverride : InvokeModuleBase, IExternalCommand { public override string ModuleName => "LinkedFileSettingsOverride"; }
-	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-	public class InvokeHideUselessReferencePlanes : InvokeModuleBase, IExternalCommand { public override string ModuleName => "HideUselessReferencePlanes"; }
 	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
 	public class InvokeAlign3DViewToScopeBox : InvokeModuleBase, IExternalCommand { public override string ModuleName => "Align3DViewToScopeBox"; }
 	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
 	public class InvokeClampFloorplanCropRegion : InvokeModuleBase, IExternalCommand { public override string ModuleName => "ClampFloorplanCropRegion"; }
 	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
 	public class InvokePanelFilterTemplates : InvokeModuleBase, IExternalCommand { public override string ModuleName => "PanelFilterTemplates"; }
+	
 	//Search projects
 	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-	public class InvokeFindElements : InvokeModuleBase, IExternalCommand { public override string ModuleName => "FindElements"; }
-	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
 	public class InvokeSelectedFindView : InvokeModuleBase, IExternalCommand { public override string ModuleName => "SelectedFindView"; }
-	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-	public class InvokeFindReplaceParameter : InvokeModuleBase, IExternalCommand { public override string ModuleName => "FindReplaceParameter"; }
-	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-	public class InvokeFindByParameterInView : InvokeModuleBase, IExternalCommand { public override string ModuleName => "FindByParameterInView"; }
+	
 	//M.P.A.C.T. projects
 	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
     public class InvokeWireBOMLite : InvokeModuleBase, IExternalCommand { public override string ModuleName => "WireBOMLite"; }
-	//Graphics projects
 	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-	public class InvokeFixOverrideColor : InvokeModuleBase, IExternalCommand { public override string ModuleName => "FixOverrideColor"; }
+    public class InvokeP3Lighting : InvokeModuleBase, IExternalCommand { public override string ModuleName => "P3Lighting"; }
+	
 	// Fixture projects
 	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
 	public class InvokeLightingFixtureElevationToStructure : InvokeModuleBase, IExternalCommand { public override string ModuleName => "LightingFixtureElevationToStructure"; }
+	
 	//Test projects
-	[Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-	public class InvokeRevitSTLExporter : InvokeModuleBase, IExternalCommand { public override string ModuleName => "RevitSTLExporter"; }
 	// [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
 	// public class InvokeRevitTestBed : InvokeModuleBase, IExternalCommand { public override string ModuleName => "RevitTestBed"; }
 }
